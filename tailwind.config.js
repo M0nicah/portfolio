4dc7fffbaa2ba75b51b85dd6320b993022a9e3eb
Monica Masae <monica.w.masae@gmail.com>
2022-10-05 18:59:42 +0300

    add tailwind hero section and about me

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+    colors: {
+      porcelain: "#e9eeee",
+      tuna: "#283543",
+      wild_willow: "#a8c267"
+    
+    }},
+  },
+  plugins: [],
+}
